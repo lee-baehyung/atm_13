d135a247d262f4a942aa213e9bf434f836f11b15
boxer <leebh0806@naver.com>
2025-07-18 13:24:32 +0900

    ✨ 영수증 보이기 추가
    영수증 내역 확인 기능 추가 및 영수증 내역 없을시 안내메시지 추가

--- atm.py
+++ atm.py
@@ -5,11 +5,12 @@
 # 기능 입력을 받는 기능을 구현해주세요
 
 balance = 10000
-
+#list  사용하기 위해 미리 만드는 것, 초기화 특정값을 넣어서 만들거나, 빈 값을 넣어서 만듭니다.
 #무한루프? while vs for 
 
 # while 조건 :
 #     조건 만족 시키는 중
+receipts = []
 
 while True:
     num = input("사용하실 기능의 번호를 선택해주세요 (1.입금,  2.출금, 3.입출금내역 영수증보기, 4.종료) : ")
@@ -21,14 +22,23 @@ while True:
        deposit_amount = int(input('입금할 금액을 입력해주세요: ')) # str:5000 -> int -> int:500
        balance += deposit_amount # balance = balance + deposit_amount
        print(f'입금하신 금액{deposit_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+       receipts.append( ('입금', deposit_amount, balance) )
        
     if num == '2':
        withdraw_amount = int(input('출금할 금액을 입력해주세요: '))
        withdraw_amount = min(balance, withdraw_amount) #min(10000, 30000) ->10000 / min(10000, 3000) ->3000
        balance -= withdraw_amount # balance = balance - withdraw_amount
        print(f'출금하신 금액{withdraw_amount}원이고, 현재 잔액은 {balance}원 입니다.')
+       receipts.append( ('출금', withdraw_amount, balance) )
 
     if num == '3':
-        pass
+        if receipts:
+            1# [('입금', 5000, 15000), ('출금', 3000, 12000), ('입금', 10000, 22000), ('입금', 20000, 42000)]
+            for i in receipts:
+            # i = ('출금1', 5000, 15000)
+            # i[0] => '출금'
+                print(f"{i[0]}: {i[1]}원 | 잔액: {i[2]}")
+        else:
+            print(f"영수증 내역이 없습니다.")
 
 print(f'서비스를 종료합니다. 현재 잔액은 {balance}')
